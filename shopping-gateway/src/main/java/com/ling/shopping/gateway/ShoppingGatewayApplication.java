3bccb5661a025fd30406800aaf23ee68fe38e343
lizuxian <tanzheng.li@outlook.com>
2025-11-22 20:24:58 +0800

    feature:init gateway

---
+++ shopping-gateway/src/main/java/com/ling/shopping/gateway/ShoppingGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.ling.shopping.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class ShoppingGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingGatewayApplication.class, args);
+    }
+
+}
---
+++ shopping-gateway/src/test/java/com/ling/shopping/gateway/GulimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ling.shopping.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class shoppingGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
